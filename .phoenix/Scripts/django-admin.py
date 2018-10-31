940123a5cbc706476ccdf848c1354dca58c28141
RICARDO SOUSA PIMENTEL <2306214@ifto.local>
2018-10-31 17:12:41 -0300

    Primeiro commit

---
+++ .phoenix/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\Projetos\Phoenix\.phoenix\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn phoenix.wsgi --log-file -
